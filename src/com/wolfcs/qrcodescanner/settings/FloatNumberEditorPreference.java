ad960e059082a17df7273eea9f89f6203fb42c58
Pengfei Han <hanpfei@gmail.com>
2014-06-14 15:40:38 +0800

    TemperatureSettingsFragment and FloatNumberEditorPreference.
    Signed-off-by: Pengfei Han <hanpfei@gmail.com>

---
+++ src/com/wolfcs/qrcodescanner/settings/FloatNumberEditorPreference.java
@@ -0,0 +1,120 @@
+package com.wolfcs.qrcodescanner.settings;
+
+import com.wolfcs.qrcodescanner.R;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.preference.DialogPreference;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class FloatNumberEditorPreference extends DialogPreference {
+    private static final String TAG = "";
+    private static final float DEFAULT_MINIMUM_VALUE = 0.0f;
+    private static final float DEFAULT_MAXIMUM_VALUE = 0.0f;
+
+    private EditText mEditText;
+    private float mMinimumValue;
+    private float mMaximumValue;
+    private float mDefaultValue;
+    private String mUnit;
+
+    public FloatNumberEditorPreference(Context context, AttributeSet attrs,
+            int defStyle) {
+        super(context, attrs, defStyle);
+        TypedArray arr = context.obtainStyledAttributes(attrs, R.styleable.wolfcs);
+        mMinimumValue = arr.getFloat(R.styleable.wolfcs_minValue, DEFAULT_MINIMUM_VALUE);
+        mMaximumValue = arr.getFloat(R.styleable.wolfcs_maxValue, DEFAULT_MAXIMUM_VALUE);
+        mDefaultValue = arr.getFloat(R.styleable.wolfcs_defaultValue, mMinimumValue);
+        mUnit = arr.getString(R.styleable.wolfcs_unit);
+    }
+
+    public FloatNumberEditorPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.dialogPreferenceStyle);
+    }
+
+    public FloatNumberEditorPreference(Context context) {
+        this(context, null);
+    }
+    
+    @Override
+    protected View onCreateDialogView() {
+        return View.inflate(getContext(), R.layout.preference_dialog_numbereditor, null);
+    }
+
+    @Override
+    protected void onBindDialogView(View view) {
+        super.onBindDialogView(view);
+        mEditText = (EditText)view.findViewById(R.id.number_editor);
+        float value = getSharedPreferences().getFloat(getKey(), mDefaultValue);
+        getEditor().putFloat(getKey(), value).commit();
+        mEditText.setText(String.valueOf(value));
+        mEditText.setSelection(String.valueOf(value).length());
+
+        mEditText.setFilters(new InputFilter[] {mInputFilter});
+    }
+
+    private InputFilter mInputFilter = new InputFilter () {
+
+        @Override
+        public CharSequence filter(CharSequence source, int start, int end,
+                Spanned dest, int dstart, int dend) {
+            String text = mEditText.getText().toString();
+            float value = 0;
+
+            try {
+                if (!text.equals("")) {
+                    value = Integer.parseInt(text);
+                }
+                value = Float.parseFloat(dest.toString() + source.toString());
+            } catch (Exception e) {
+                Log.w(TAG, "parseInt failed: string = " + dest.toString() + source.toString());
+            }
+
+            if (value < mMinimumValue) {
+                return "";
+            }
+
+            if (value > mMaximumValue) {
+                return "";
+            }
+
+            return source;
+        }
+        
+    };
+
+    @Override
+    protected void onDialogClosed(boolean positiveResult) {
+        if (positiveResult) {
+            String text = mEditText.getText().toString();
+            float value = Float.valueOf(text);
+            if (callChangeListener(value)) {
+                // To save value;
+                saveValue(value);
+            }
+        }
+    }
+
+    protected boolean needInputMethod() {
+        // We want the input method to show, if possible, when dialog is displayed
+        return true;
+    }
+
+    private void saveValue(float value) {
+        getEditor().putFloat(getKey(), value).commit();
+        notifyChanged();
+    }
+
+    public float getValue() {
+        return getSharedPreferences().getFloat(getKey(), mDefaultValue);
+    }
+
+    public String getUnit() {
+        return mUnit;
+    }
+}
--- src/com/wolfcs/qrcodescanner/settings/TemperatureSettingsFragment.java
+++ src/com/wolfcs/qrcodescanner/settings/TemperatureSettingsFragment.java
@@ -7,18 +7,24 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceFragment;
-import android.util.Log;
+import android.preference.SwitchPreference;
 
 public class TemperatureSettingsFragment extends PreferenceFragment 
     implements OnPreferenceChangeListener {
-    private static final String TAG = "TemperatureSettingsFragment";
-    private static final boolean DEBUG = true;
 
     private static final String TEMP_RANGE_KEY = "temp_range";
     private static final String RANGE_MODE_KEY = "range_model";
-    
+    private static final String ABOVE_ALARM_KEY = "above_alarm";
+    private static final String ABOVE_THRESHOLD_KEY = "above_threshold";
+    private static final String BELOW_ALARM_KEY = "below_alarm";
+    private static final String BELOW_THRESHOLD_KEY = "below_threshold";
+
     private ListPreference mTempRangePreference;
     private ListPreference mRangeModePreference;
+    private SwitchPreference mAboveAlarmPreference;
+    private FloatNumberEditorPreference mAboveThresholdPreference;
+    private SwitchPreference mBelowAlarmPreference;
+    private FloatNumberEditorPreference mBelowThresholdPreference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,31 +35,62 @@ public class TemperatureSettingsFragment extends PreferenceFragment
         mTempRangePreference.setOnPreferenceChangeListener(this);
         String value = mTempRangePreference.getValue();
         int  valueIndex = Integer.parseInt(value);
-        mTempRangePreference.setSummary(mTempRangePreference.getEntries()[valueIndex]);
-        mTempRangePreference.setValue(value);
+        updateListPreference(mTempRangePreference, valueIndex);
 
         mRangeModePreference = (ListPreference)findPreference(RANGE_MODE_KEY);
         mRangeModePreference.setOnPreferenceChangeListener(this);
         value = mRangeModePreference.getValue();
         valueIndex = Integer.parseInt(value);
-        mRangeModePreference.setSummary(mRangeModePreference.getEntries()[valueIndex]);
+        updateListPreference(mRangeModePreference, valueIndex);
         mRangeModePreference.setValue(value);
 
-//        mThermalSensorController = ThermalSensorController.getInstance();
+        mAboveAlarmPreference = (SwitchPreference) findPreference(ABOVE_ALARM_KEY);
+        mAboveAlarmPreference.setOnPreferenceChangeListener(this);
+        mAboveThresholdPreference = (FloatNumberEditorPreference)
+                findPreference(ABOVE_THRESHOLD_KEY);
+        mAboveThresholdPreference.setOnPreferenceChangeListener(this);
+        updateAboveAlarmPreference();
+
+        mBelowAlarmPreference = (SwitchPreference) findPreference(BELOW_ALARM_KEY);
+        mBelowAlarmPreference.setOnPreferenceChangeListener(this);
+        mBelowThresholdPreference = (FloatNumberEditorPreference)
+                findPreference(BELOW_THRESHOLD_KEY);
+        mBelowThresholdPreference.setOnPreferenceChangeListener(this);
+    }
+
+    private void updateListPreference(ListPreference preference, int value) {
+        preference.setSummary(preference.getEntries()[value]);
+    }
+
+    private void updateAboveAlarmPreference() {
+        if (mAboveAlarmPreference.isChecked()) {
+            mAboveThresholdPreference.setEnabled(true);
+        } else {
+            mAboveThresholdPreference.setEnabled(false);
+        }
+
+        String unit = mAboveThresholdPreference.getUnit();
+        CharSequence title = mAboveThresholdPreference.getDialogTitle();
+        mAboveThresholdPreference.setDialogTitle(title + "(" + unit + ")");
+
+        float value = mAboveThresholdPreference.getValue();
+        mAboveThresholdPreference.setSummary(value + unit);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (DEBUG) Log.i(TAG, "onPreferenceChange: Preference " + preference + " Value " + newValue);
         if(preference == mTempRangePreference) {
             int  valueIndex = Integer.parseInt((String)newValue);
-            mTempRangePreference.setSummary(mTempRangePreference.getEntries()[valueIndex]);
-            mTempRangePreference.setValue((String)newValue);
+            updateListPreference(mTempRangePreference, valueIndex);
+            return true;
         } else if (preference == mRangeModePreference) {
             int  valueIndex = Integer.parseInt((String)newValue);
-//            mThermalSensorController.setRangeMode(valueIndex);
-            mRangeModePreference.setSummary(mRangeModePreference.getEntries()[valueIndex]);
-            mRangeModePreference.setValue((String)newValue);
+            updateListPreference(mRangeModePreference, valueIndex);
+            return true;
+        } else if(preference == mAboveAlarmPreference) {
+            return true;
+        } else if(preference == mBelowAlarmPreference) {
+            return true;
         }
         return false;
     }
